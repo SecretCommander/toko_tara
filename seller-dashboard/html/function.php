14941ed85a86ca1b746bed9b4f603782eaba339d
F4RR3L <146092030+SecretCommander@users.noreply.github.com>
2023-12-08 11:23:15 +0700

    Add files via upload

---
+++ seller-dashboard/html/function.php
@@ -0,0 +1,219 @@
+<?php
+
+require 'koneksi.php';
+//Proses Input Vidio
+function uvid($vidio)
+{
+    if (isset($vidio['name'])) {
+        $sizevid = $vidio['size'];
+        $split = explode('.', $vidio['name']);
+        $ext = $split[count($split) - 1];
+        if ($sizevid > 50000000){
+            echo "<script>
+            alert('Size Terlalu Gede!');
+            </script>";
+            return false;
+        }
+
+        $vid = uniqid() . "." . $ext;
+
+        $dir = "video/";
+        $tmpfile = $vidio['tmp_name'];
+
+        move_uploaded_file($tmpfile, $dir . $vid);
+        return $vid;
+    } else {
+        return null;
+    }
+}
+//Proses Input Gambar 
+function upload($gambar,$input)
+{
+    if (isset($gambar['name'])) {
+        $error = $gambar['error'];
+        $split = explode('.', $gambar['name']);
+        $ext = $split[count($split) - 1];
+
+        $fot = uniqid() . "." . $ext;
+        if ($input == 'foto') {
+            if($error === 4){
+                echo "<script>
+                alert('pilih gambar terlebih dahulu');
+                </script>";
+                return false;
+            }    
+        } 
+        
+
+        $dir = "imgbarang/";
+
+        
+
+        $tmpfile = $gambar['tmp_name'];
+
+        move_uploaded_file($tmpfile, $dir . $fot);
+        return $fot;
+    } else {
+        return null;
+    }
+}
+//Input Barang Toko
+function input_data()
+{
+    if (!isset($_SESSION['penjual_id']) || empty($_SESSION['penjual_id'])) {
+        echo "<script>alert('Login Terlebih Dahulu'); window.location.href='../penjual/login.php';</script>";
+        die();
+    }
+
+    $id_penjual = $_SESSION['penjual_id'];
+    global $conn;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nama_produk = htmlspecialchars($_POST['np']);
+        $Stok_produk = $_POST['jlh'];
+        $berat = $_POST['berat'];
+        $panjang = $_POST['panjang'];
+        $lebar = $_POST['lebar'];
+        $tinggi = $_POST['tinggi'];
+        function belahkah(){
+            if (isset($_POST['belah']) && !empty($_POST['belah'])){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        $belah = belahkah();
+
+        $vidioo = uvid($_FILES['vidi']);
+        $foto_produk = upload($_FILES['foto'], "foto");
+        $foto2 = upload($_FILES['foto1'], "foto1");
+        $foto3 = upload($_FILES['foto2'],"foto1");
+
+        $kategori = $_POST['kate'];
+        $harga = 25000;
+        $berat = $_POST['berat'];
+        $deskripsi = htmlspecialchars($_POST['deskripsi']);
+
+
+        // Perform the SQL query to insert the data into the database
+        $sql = "INSERT INTO barang (nama_produk, jumlah_produk, foto_produk, kategori_produk, harga_produk, berat, deskripsi_produk, id_toko, vid, foto2, foto3,  panjang, lebar, tinggi, belah) 
+            VALUES ('$nama_produk', '$Stok_produk', '$foto_produk', '$kategori', '$harga', '$berat', '$deskripsi', '$id_penjual', '$vidioo','$foto2', '$foto3','$panjang','$lebar','$tinggi', '$belah')";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<script>alert('Data berhasil dimasukkan'); window.location.href='tampil_barang.php';</script>";
+            mysqli_close($conn);
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+}
+//Menampilkan Data Barang
+function detail_data($var_id)
+{
+    global $conn;
+    global $result;
+    $sql = "SELECT *FROM barang WHERE id_produk=?";
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        $param_id = $var_id;
+        if (mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_get_result($stmt);
+            if (mysqli_num_rows($result) == 1) {
+                return true; //jika ada data nilai true
+            } else {
+                return false; //jika data tidak ditemukan nilai false
+            }
+        } else {
+            echo "Terjadi kesalahan";
+        }
+    }
+    mysqli_stmt_close($stmt);
+}
+
+//Delete Barang
+function delete_item($var_id)
+{
+    $idf = $_GET["id"];
+    global $conn;
+
+    $queryShow = "SELECT * FROM barang where id_produk = '$idf';";
+    $sqlShow = mysqli_query($conn, $queryShow);
+    $result = mysqli_fetch_assoc($sqlShow);
+
+    unlink("img/" . $result['foto_produk']);
+
+    $sql = "DELETE FROM barang WHERE id_produk = ?";
+
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_bind_param($stmt, "i", $var_id);
+
+        if (mysqli_stmt_execute($stmt)) {
+            echo "<script>alert('Item deleted successfully'); window.location.href='tampil_barang.php';</script>";
+        } else {
+            echo "Error deleting item";
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+}
+//Update Data
+function update_data()
+{
+    global $conn;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // menyimpan data kedalam variabel
+        $id = $_POST['id_produk'];
+        $nama_produk = htmlspecialchars($_POST['nama_produk']);
+        $Stok_produk = $_POST['Stok_produk'];
+        $kategori_produk = htmlspecialchars($_POST['kategori_produk']);
+        $harga_produk = $_POST['harga_produk'];
+        $berat = $_POST['berat'];
+        $deskripsi_produk = htmlspecialchars($_POST['deskripsi_produk']);
+
+        $queryShow = "SELECT * FROM barang WHERE id_produk = '$id';";
+        $sqlShow = mysqli_query($conn, $queryShow);
+        $result = mysqli_fetch_assoc($sqlShow);
+
+        if ($_FILES["foto_produk"]["name"] == "") {
+            $foto = $result["foto_produk"];
+        } else {
+            $split = explode('.', $_FILES['foto_produk']['name']);
+            $ext = $split[count($split) - 1];
+
+            $foto = uniqid() . "." . $ext;
+            unlink("img/" . $result['foto_produk']);
+            move_uploaded_file($_FILES["foto_produk"]["tmp_name"], "img/" . $foto);
+        }
+
+        // query SQL untuk insert data ke dalam Mysql
+        $sql = "UPDATE barang SET nama_produk='$nama_produk', jumlah_produk='$Stok_produk', kategori_produk='$kategori_produk',
+foto_produk='$foto', harga_produk='$harga_produk', berat='$berat', deskripsi_produk='$deskripsi_produk' where id_produk='$id'";
+        mysqli_query($conn, $sql);
+        // mengalihkan ke halaman index.php
+        echo "<script>alert('Data yang anda Update sukses');window.location='tampil_barang.php'</script>";
+    }
+}
+//Menampilkan Semua Barang
+function tampil_data()
+{
+    global $conn;
+    $sql = "SELECT * FROM barang";
+    $result = mysqli_query($conn, $sql);
+    return $result;
+}
+//Function Barang sesuai Toko
+function tampil_barangtoko($idtoko)
+{
+    global $conn;
+    $sql = "SELECT * FROM barang WHERE id_toko= '$idtoko'";//id toko
+    $result = mysqli_query($conn, $sql);
+    return $result;
+}
+//Function Search
+function searching($keyword)
+{
+    global $conn;
+    $sql = "SELECT * FROM barang where nama_produk LIKE '%$keyword%' ";
+    $result = mysqli_query($conn, $sql);
+    return $result;
+}
---
+++ seller-dashboard/html/list-produk.php
@@ -0,0 +1,362 @@
+<?php
+session_start();
+include 'koneksi.php';
+include 'function.php';
+
+if (!isset($_SESSION['penjual_id'])) {
+    header("location: ../../penjual/login.php");
+}
+$result = tampil_barangtoko($_SESSION['penjual_id']);
+?>
+<!DOCTYPE html>
+
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="../assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <link rel="stylesheet" href="../assets/vendor/libs/apex-charts/apex-charts.css" />
+
+    <!-- Page CSS -->
+
+    <!-- Helpers -->
+    <script src="../assets/vendor/js/helpers.js"></script>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../assets/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+
+        <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+          <div class="app-brand demo">
+            <a href="index.html" class="app-brand-link">
+              <img src="../assets/img/tara.png" alt="" width="190px">
+              <!-- <span class="app-brand-text demo menu-text fw-bolder ms-2">Sneat</span> -->
+            </a>
+
+            <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+              <i class="bx bx-chevron-left bx-sm align-middle"></i>
+            </a>
+          </div>
+
+          <div class="menu-inner-shadow"></div>
+
+          <ul class="menu-inner py-1">
+            <li class="menu-item">
+              <a href="index.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                <div>Rumah</div>
+              </a>
+            </li>
+            <li class="menu-item">
+              <a href="toko.html" class="menu-link">
+                <i class="menu-icon tf-icons bx bx-store-alt"></i>
+                <div>Toko</div>
+              </a>
+            </li>
+            <li class="menu-item active open">
+              <a href="javascript:void(0)" class="menu-link menu-toggle">
+                <i class="menu-icon tf-icons bx bx-detail"></i>
+                <div>Produk</div>
+              </a>
+              <ul class="menu-sub">
+                <li class="menu-item">
+                  <a href="tambah-produk.php" class="menu-link">
+                    <div>Tambah Produk</div>
+                  </a>
+                </li>
+                <li class="menu-item active">
+                  <a href="list-produk.php" class="menu-link">
+                    <div>List Produk</div>
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </aside>
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+
+          <nav
+            class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+            id="layout-navbar"
+          >
+            <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
+              <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
+                <i class="bx bx-menu bx-sm"></i>
+              </a>
+            </div>
+
+            <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+              <!-- Search -->
+              <div class="navbar-nav align-items-center">
+                <div class="nav-item d-flex align-items-center">
+                  <i class="bx bx-search fs-4 lh-0"></i>
+                  <input
+                    type="text"
+                    class="form-control border-0 shadow-none"
+                    placeholder="Search..."
+                    aria-label="Search..."
+                  />
+                </div>
+              </div>
+              <!-- /Search -->
+
+              <ul class="navbar-nav flex-row align-items-center ms-auto">
+                <!-- User -->
+                <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                  <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                    <div class="avatar avatar-online">
+                      <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                    </div>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        <div class="d-flex">
+                          <div class="flex-shrink-0 me-3">
+                            <div class="avatar avatar-online">
+                              <img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                            </div>
+                          </div>
+                          <div class="flex-grow-1">
+                            <span class="fw-semibold d-block">John Doe</span>
+                            <small class="text-muted">Admin</small>
+                          </div>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        <i class="bx bx-user me-2"></i>
+                        <span class="align-middle">My Profile</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        <i class="bx bx-cog me-2"></i>
+                        <span class="align-middle">Settings</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        <span class="d-flex align-items-center align-middle">
+                          <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
+                          <span class="flex-grow-1 align-middle">Billing</span>
+                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
+                        </span>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="auth-login-basic.html">
+                        <i class="bx bx-power-off me-2"></i>
+                        <span class="align-middle">Log Out</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <!--/ User -->
+              </ul>
+            </div>
+          </nav>
+
+          <!-- / Navbar -->
+          
+          <!-- Content wrapper -->
+          <div class="content-wrapper">
+            <div class="container-xxl flex-grow-1 container-p-y">
+              <div class="row">
+                <div class="col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <h4>List Produk</h4>
+                      <p class="mt-n2">produk-produk yang anda tampilkan</p>
+                    </div>
+          <?php 
+            if($result){
+              $num = 0;
+              if (mysqli_num_rows($result) > 0){
+                echo "<div class='card-body'>";
+                echo "<div class='table-responsive text-nowrap'>";
+                echo "<table class='table table-hover'>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th width='50px'>No</th>";
+                echo "<th>Gambar</th>";
+                echo "<th>Nama Barang</th>";
+                echo "<th>Harga</th>";
+                echo "<th>Jumlah Stok</th>";
+                echo "<th>Satuan</th>";
+                echo "<th>Date</th>";
+                echo "<th style='text-align: center;'>Aksi</th>";
+                echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+              }
+              while ($row = mysqli_fetch_array($result)) {
+                $num++;
+                echo "<tr>";
+                echo "<td>{$num}</td>";
+                echo "<td><img src='imgbarang/" . $row['foto_produk'] . "'style='width:100px;'></td>";
+                echo "<td> ". $row['nama_produk'] . "</td>";
+                echo "<td> " . $row['harga_produk'] . "</td>";
+                echo "<td>" . $row['jumlah_produk'] . "</td>";
+                echo "<td>stok</td>";
+                echo "<td>" . $row['tgl_upload'] . "</td>";
+                echo "<td>";
+                echo "<div class='dropdown'>";
+                echo "<button type='button' class='btn p-0 dropdown-toggle hide-arrow' data-bs-toggle='dropdown'>";
+                echo "<i class='bx bx-dots-vertical-rounded'></i>";
+                echo "</button>";
+                echo "<div class='dropdown-menu'>";
+                echo "<a class='dropdown-item' href='../../barang/update_barang.php?id=" . $row['id_produk'] . "'><i class='bx bx-edit-alt me-1'></i> Edit</a>";
+                echo "<a class='dropdown-item' href='../../barang/delete.php?id=" . $row['id_produk'] . "'><i class='bx bx-trash me-1'></i> Delete</a>";
+                echo "</div>";
+                echo "</div></td></tr>";
+              }
+            }
+          ?>                                                     
+                            <tr>
+                              <td>2</td>
+                              <td>madu.jpg</td>
+                              <td>Madu Tara</td>
+                              <td>Rp50.000</td>
+                              <td>98</td>
+                              <td>botol</td>
+                              <td>28-11-23</td>
+                              <td>
+                                <div class="dropdown">
+                                  <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                    <i class="bx bx-dots-vertical-rounded"></i>
+                                  </button>
+                                  <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                                    <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                                  </div>
+                                </div>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- Footer -->
+              <!-- <footer class="content-footer footer bg-footer-theme">
+                <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                  <div class="mb-2 mb-md-0">
+                    ©
+                    <script>
+                      document.write(new Date().getFullYear());
+                    </script>
+                    , made with ❤️ by
+                    <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+                  </div>
+                  <div>
+                    <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                    <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+
+                    <a
+                      href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
+                      target="_blank"
+                      class="footer-link me-4"
+                      >Documentation</a
+                    >
+
+                    <a
+                      href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
+                      target="_blank"
+                      class="footer-link me-4"
+                      >Support</a
+                    >
+                  </div>
+                </div>
+              </footer> -->
+              <!-- / Footer -->
+            </div>
+            <div class="content-backdrop fade"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Overlay -->
+      <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="../assets/vendor/js/script.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="../assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="../assets/js/dashboards-analytics.js"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
